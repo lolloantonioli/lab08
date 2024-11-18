4f0ffdcd678aa35166ec78acf081c8b2a5e15492
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2024-11-18 11:25:40 +0100

    Finished exercise 83

--- 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -1,14 +1,20 @@
 package it.unibo.mvc;
 
+import java.lang.Class;
+import java.lang.reflect.InvocationTargetException;
+
 import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
-import it.unibo.mvc.view.DrawNumberSwingView;
 
 /**
  * Application entry-point.
  */
 public final class LaunchApp {
+    
+    private static final String FIRST_NAME = "it.unibo.mvc.view.DrawNumberSwingView";
+    private static final String SECOND_NAME = "it.unibo.mvc.view.DrawNumberViewOut";
 
     private LaunchApp() { }
 
@@ -23,9 +29,32 @@ public final class LaunchApp {
      * @throws IllegalAccessException in case of reflection issues
      * @throws IllegalArgumentException in case of reflection issues
      */
-    public static void main(final String... args) {
+    public static void main(final String... args)
+        throws 
+        ClassNotFoundException, 
+        NoSuchMethodException,
+        InvocationTargetException,
+        InstantiationException, 
+        IllegalAccessException {
+
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
-        app.addView(new DrawNumberSwingView());
+        final var firstClazz = Class.forName(FIRST_NAME);
+        final var secondClazz = Class.forName(SECOND_NAME);
+        for (int i = 0; i < 3; i++) {
+            final DrawNumberView firstClazzView = (DrawNumberView)firstClazz.getConstructor().newInstance();
+            if (DrawNumberView.class.isAssignableFrom(firstClazzView.getClass())) {
+                app.addView(firstClazzView);
+            } else {
+                throw new IllegalArgumentException();
+            }
+            
+            final DrawNumberView secondClazzView = (DrawNumberView)secondClazz.getConstructor().newInstance();
+            if (DrawNumberView.class.isAssignableFrom(secondClazzView.getClass())) {
+                app.addView(secondClazzView);
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
